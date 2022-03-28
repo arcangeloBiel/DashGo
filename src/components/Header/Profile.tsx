9422190248b4cb6a1ff491b843d979607443493d
joao <joao.ribeiro@havan.com.br>
2022-03-28 14:49:16 -0300

    Sidebar responsiva

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,22 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
+interface ProfileProps {
+  showProfielData?: boolean;
+}
 
-export function Profile() {
+export function Profile({ showProfielData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="center">
-        <Text>Jaca</Text>
-        <Text color="gray.300" fontSize="small">
-          Jaca@gmail.com
-        </Text>
-      </Box>
+      { showProfielData && (
+        <Box mr="4" textAlign="center">
+          <Text>Jaca</Text>
+          <Text color="gray.300" fontSize="small">
+            Jaca@gmail.com
+          </Text>
+        </Box>
+      )}
+      
+
       <Avatar
         size="md"
         name="JACA RIBEIRO"
