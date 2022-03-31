3c0be2d23dffd61d98e60c97dfee4415b8aa2a68
joao <joao.ribeiro@havan.com.br>
2022-03-30 21:16:34 -0300

    finaliza parte 1

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,31 +1,33 @@
-import {
-    Input as ChakraInput,
-    InputProps as ChakraInputProps,
-    FormLabel,
-    FormControl,
-  } from "@chakra-ui/react";
+import { Input as ChakraInput, InputProps as ChakraInputProps, FormLabel, FormControl } from "@chakra-ui/react";
+import { forwardRef } from "react";
 
-  interface Inputprops extends ChakraInputProps {
-      name: string;
-      label?: string
-  }
-
-export function Input({name, label, ...rest }: Inputprops) {
-    return(
-        <FormControl>
-      { !!label && <FormLabel htmlFor={name}>{ label }</FormLabel>}
-        <ChakraInput
-          id={name}
-          name={name}
-          focusBorderColor="pink.500"
-          bgColor="gray.900"
-          variant="flushed"
-          _hover={{
-            bgColor: "gray.900",
-          }}
-          size="lg"
-          {...rest}
-        />
-      </FormControl>
-    )
+interface Inputprops extends ChakraInputProps {
+  name: string;
+  label?: string;
 }
+
+const InputBase = ({ name, label, ...rest }: Inputprops, ref) => {
+  return (
+  
+    <FormControl>
+      
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+     
+      <ChakraInput
+        id={name}
+        name={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="flushed"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        ref={ref}
+        {...rest}
+      />
+    </FormControl>
+  );
+};
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+type SignFormData = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  const { register, handleSubmit, formState, formState: { errors }} = useForm();
+  console.log('err', errors)
+
+  const handleSignIn: SubmitHandler<SignFormData> = async (data) => {
+    await new Promise(resolve => setTimeout(resolve, 20));
+    close()
+    console.log(data);
+  
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,16 +28,33 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-        
-        <Input name="email" type="email" label="email" />
-        <Input name="password" type="password" label="senha" />
-
-        
+          <Input
+            name="email"
+            type="email"
+            label="email"
+          
+            {...register("email", {required: 'E-mail obrigatório'})}
+          />
+          {errors.email && <span>E-mail é obrigatório</span>}
+          <Input
+            name="password"
+            type="password"
+            label="senha"
+            {...register("password", {required: 'Senha obrigatório'})}
+          />
+           {errors.password && <span>Senha é obrigatório</span>}
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitted}
+        >
           Entrar
         </Button>
       </Flex>
