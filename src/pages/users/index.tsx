99a45472ae2d240d767e03b8ac88d3f45d17b5e3
joao <joao.ribeiro@havan.com.br>
2022-04-03 10:43:47 -0300

    Criando hook de listagem

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,24 +22,10 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination/Index";
 import Link from "next/link";
 import { api } from "../../services/api";
+import { userUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery("users", async () => {
-    const { data } = await api.get("users");
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
-    return users;
-  });
+  const { data, isLoading, isFetching, error } = userUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
